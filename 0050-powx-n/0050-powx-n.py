9d39486bf3f6666d4d9473c4de2511b1039a5dea
Sasi Akula <sasiakula006@gmail.com>
2026-02-01 21:53:47 +0530

    Time: 0 ms (100%), Space: 12.6 MB (31.34%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def myPow(self, x, n):
+        if n==0:
+            return 1
+        elif n<0:
+            return 1/self.myPow(x,-n)
+        else:
+            if n%2:
+                return x*self.myPow(x,n-1)
+            else:
+                return self.myPow(x*x,n/2)        
