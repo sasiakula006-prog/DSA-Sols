5247d2d84ba568f5310846d349d4b297808303bf
Sasi Akula <sasiakula006@gmail.com>
2026-03-15 23:58:09 +0530

    Time: 4 ms (89.95%), Space: 12.4 MB (92.43%) - LeetHub

---
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def combinationSum2(self, candidates, target):
+        L=[]
+        candidates.sort()
+        def srh(s,i,t):
+            if t==0:
+                L.append(s)
+                return 
+            if t<0:
+                return
+            for j in range(i,len(candidates)):
+                if j>i and candidates[j]==candidates[j-1]:
+                    continue
+                if candidates[j] > t:
+                    break
+                srh(s+[candidates[j]],j+1,t-candidates[j])
+        srh([],0,target)
+        return L
+
+
+        
