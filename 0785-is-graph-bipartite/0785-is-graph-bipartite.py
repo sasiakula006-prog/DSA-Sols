5fe2ee4fdcded884ff34a2fca4dfd39fc2091d25
Sasi Akula <sasiakula006@gmail.com>
2026-04-16 21:43:41 +0530

    Time: 4 ms (24.92%), Space: 12.6 MB (79.97%) - LeetHub

---
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def isBipartite(self, graph):
+        n = len(graph)
+        vis = [False]*n
+        c = [0]*n
+        for i in range(n):
+            if not vis[i]:
+                q = deque()
+                q.append(i)
+                vis[i] = True
+                c[i]=0 
+                while q:
+                    a = q.popleft()
+                    for v in graph[a]:
+                        if not vis[v]:
+                            vis[v] = True
+                            c[v] = (1+c[a])%2
+                            q.append(v)
+                        else:
+                            if c[v] == c[a]:
+                                return False
+        return True
