d6ad81c41c6a6a94636265fcb23d27c2f464a489
Sasi Akula <sasiakula006@gmail.com>
2026-05-29 23:19:54 +0530

    Time: 66 ms (27.06%), Space: 12.9 MB (11.93%) - LeetHub

---
+++ 0778-swim-in-rising-water/0778-swim-in-rising-water.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def swimInWater(self, grid):
+        n = len(grid)
+        dic = [(0,1),(1,0),(-1,0),(0,-1)]
+        h = [(grid[0][0],0,0)]
+        t=0
+        vis = set()
+        while h:
+            l,i,j = heapq.heappop(h)
+            vis.add((i,j))
+            if t<l:
+                t=l
+            if (i,j)==(n-1,n-1):
+                return t
+            for di,dj in dic:
+                ni,nj = i+di,j+dj
+                if 0<=ni<n and 0<=nj<n and (ni,nj) not in vis:
+                    heapq.heappush(h,(grid[ni][nj],ni,nj))
+        
+
+
+
+        
