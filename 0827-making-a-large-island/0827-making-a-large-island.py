7c984b33966a8d72972fb7b785a36801b0baae14
Sasi Akula <sasiakula006@gmail.com>
2026-05-29 16:37:08 +0530

    Time: 610 ms (99.12%), Space: 42 MB (15.04%) - LeetHub

--- 0827-making-a-large-island/0827-making-a-large-island.py
+++ 0827-making-a-large-island/0827-making-a-large-island.py
@@ -1,4 +1,4 @@
-class Unionset:
+'''class Unionset:
     def __init__(self,n):
         self.parent = [i for i in range(n)]
         self.size = [1]*n
@@ -20,12 +20,57 @@ class Unionset:
             self.size[pu] += self.size[pv]
         else:
             self.parent[pu] = pv
-            self.size[pv] += self.size[pu]
+            self.size[pv] += self.size[pu]'''
+
+
+
+
+
+
+
+
+
 
 
 class Solution(object):
     def largestIsland(self, grid):
         m,n = len(grid),len(grid[0])
+        dic = [(0,1),(1,0),(-1,0),(0,-1)]
+        def dfs(i,j,b):
+            t = 1
+            for di,dj in dic:
+                ni,nj = i+di,j+dj
+                if 0<=ni<m and 0<=nj<n:
+                    if grid[ni][nj]==1:
+                        grid[ni][nj]=-1
+                        t+=dfs(ni,nj,b)
+                    elif grid[ni][nj]==0:
+                        b.add((ni,nj))
+            return t
+        zeros = defaultdict(int)
+        maxi = 1
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j]==1:
+                    grid[i][j] = -1
+                    boundaries = set()
+                    size = dfs(i,j,boundaries)
+                    maxi = max(maxi,size)
+                    for z in boundaries:
+                        zeros[z]+=size
+        if len(zeros):
+            return max(zeros.values())+1
+        else:
+            return maxi
+
+
+
+
+
+
+
+
+        '''m,n = len(grid),len(grid[0])
         og = Unionset(m*n)
         dic = [(0,1),(1,0),(-1,0),(0,-1)]
         zeros = []
@@ -55,5 +100,5 @@ class Solution(object):
                 a+= og.size[p]
             if ans<a:
                 ans =a  
-                
-        return ans
+
+        return ans'''
