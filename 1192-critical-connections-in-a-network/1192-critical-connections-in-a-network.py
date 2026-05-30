8ab55d0a7621a9bc957cef861337c354b1c0b475
Sasi Akula <sasiakula006@gmail.com>
2026-05-30 21:23:30 +0530

    Time: 358 ms (88.72%), Space: 83.2 MB (90.23%) - LeetHub

---
+++ 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def criticalConnections(self, n, connections):
+        if len(connections) <= n-1:
+            return connections
+        adj = [[] for _ in range(n)]
+        for u0,v0 in connections:
+            adj[u0].append(v0)
+            adj[v0].append(u0)
+        time = [1]*n
+        l_t = [1]*n
+        vis = [False]*n
+        ans = []
+        def dfs(u,p):
+            vis[u] = True
+            for v in adj[u]:
+                if v==p:
+                    continue
+                if not vis[v]:
+                    l_t[v] = time[v] = time[u]+1
+                    dfs(v,u)
+                    l_t[u] = min(l_t[u],l_t[v])
+                    if time[u]<l_t[v]:
+                        ans.append([u,v])
+                else:
+                    l_t[u] = min(l_t[u],l_t[v])
+        dfs(0,-1)
+        return ans
+
+
+        
