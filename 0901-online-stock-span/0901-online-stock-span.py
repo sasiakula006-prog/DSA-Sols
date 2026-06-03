957b2545ffec71b1da4e394f68706daa7222074b
Sasi Akula <sasiakula006@gmail.com>
2026-06-03 21:57:25 +0530

    Time: 89 ms (90.8%), Space: 17 MB (60.22%) - LeetHub

---
+++ 0901-online-stock-span/0901-online-stock-span.py
@@ -0,0 +1,13 @@
+class StockSpanner(object):
+
+    def __init__(self):
+        self.stocks = []
+    def next(self, price):
+        span=1
+        while self.stocks and self.stocks[-1][0]<=price:
+            span += self.stocks[-1][1]
+            self.stocks.pop()
+        self.stocks.append([price,span])
+        return span
+        
+        
