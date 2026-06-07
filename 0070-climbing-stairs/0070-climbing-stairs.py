17b4ef427fee22fe98c43de23c25d47d075bbc1f
Sasi Akula <sasiakula006@gmail.com>
2026-06-07 23:50:47 +0530

    Time: 0 ms (100%), Space: 12.2 MB (90.39%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,9 +1,9 @@
 class Solution(object):
     def climbStairs(self, n):
-        if n<=2:
+        if n<2:
             return n
-        momo = [0,1,2]
-        for i in range(3,n):
+        momo = [1,1]
+        for i in range(2,n):
             val = momo[i-1]+momo[i-2]
             momo.append(val)
         return momo[n-1]+momo[n-2]
