5b98752c34ff0f92e8b85a446b8dcb5da0c23284
Sasi Akula <sasiakula006@gmail.com>
2026-06-09 00:57:36 +0530

    Time: 3 ms (25.67%), Space: 12.5 MB (28.6%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def uniquePaths(self, m, n):
+        c = []
+        dic = [(0,1),(1,0)]
+        dp = [[0]*n for _ in range(m)]
+        def solve(i,j):
+            if i == m-1 and j== n-1:
+                return 1
+            if dp[i][j]:
+                return dp[i][j]
+            for di,dj in dic:
+                ni,nj = i+di,j+dj
+                if ni <m and nj < n:
+                    dp[i][j] += solve(ni,nj)
+            return dp[i][j]
+        return solve(0,0)%(2*10**9)
