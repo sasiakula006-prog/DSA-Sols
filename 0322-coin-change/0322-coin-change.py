5b86d2db1cb3e3200b4a453c7d7e417e221767ab
Sasi Akula <sasiakula006@gmail.com>
2026-06-10 18:52:07 +0530

    Time: 1222 ms (20.44%), Space: 16.7 MB (15.53%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -2,7 +2,7 @@ class Solution(object):
     def coinChange(self, coins, amount):
         l = len(coins)
         dp = [[-1]*(amount+1) for _ in range(l)]
-        def f(i,a):
+        '''def f(i,a):
             if i ==l-1:
                 if not a%coins[-1]:
                     return a/coins[-1]
@@ -17,6 +17,25 @@ class Solution(object):
             dp[i][a] = min(p1,p2)
             return min(p1,p2)
         ans = f(0,amount)
+        if ans >= 1e9:
+            return -1
+        return ans'''
+
+#tablation
+        for t in range(amount+1):
+            if not t%coins[0]:
+                dp[0][t] = t/coins[0]
+            else:
+                dp[0][t] = 1e9
+
+        for i in range(1,l):
+            for a in range(amount+1):
+                p1 = 0 + dp[i-1][a]
+                p2 = 1e9
+                if coins[i]<=a:
+                    p2 = 1+dp[i][a-coins[i]]
+                dp[i][a] = min(p1,p2)
+        ans = dp[l-1][amount]
         if ans >= 1e9:
             return -1
         return ans
