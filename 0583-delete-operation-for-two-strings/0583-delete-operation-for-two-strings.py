1b84d00aa99faa768c8c83d634999d853d1d74b2
Sasi Akula <sasiakula006@gmail.com>
2026-06-12 22:09:15 +0530

    Time: 192 ms (27.73%), Space: 14.5 MB (60%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.py
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.py
@@ -7,13 +7,10 @@ class Solution(object):
             dp[i][0] = 0
         for i in range(n+1):
             dp[0][i] = 0
-        def f(s1,s2,m,n):
-            for i in range(1,m+1):
-                for j in range(1,n+1):
-                    if s1[i-1] == s2[j-1]:
-                        dp[i][j] = 1+dp[i-1][j-1]
-                    else:
-                        dp[i][j] = max(dp[i][j-1],dp[i-1][j])
-            return dp[-1][-1]
-        l = f(word1,word2,m,n)
-        return m+n-2*l
+        for i in range(1,m+1):
+            for j in range(1,n+1):
+                if word1[i-1] == word2[j-1]:
+                    dp[i][j] = 1+dp[i-1][j-1]
+                else:
+                    dp[i][j] = max(dp[i][j-1],dp[i-1][j]) 
+        return m+n-2*(dp[-1][-1])
