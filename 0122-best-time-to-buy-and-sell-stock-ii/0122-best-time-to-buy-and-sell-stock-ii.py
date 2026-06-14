308b854d8904ede00baa60db0345235fea026c99
Sasi Akula <sasiakula006@gmail.com>
2026-06-14 14:28:54 +0530

    Time: 23 ms (5%), Space: 17.2 MB (7.49%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -2,7 +2,7 @@ class Solution(object):
     def maxProfit(self, prices):
         n = len(prices)
         dp = [[-1]*2 for _ in range(n)]
-        def f(i,b):
+        '''def f(i,b):
             if i==n:
                 return 0
             if dp[i][b] != -1:
@@ -13,4 +13,10 @@ class Solution(object):
             else:
                 dp[i][b] = max(prices[i]+f(i+1,1),f(i+1,0))
                 return dp[i][b]
-        return f(0,1)
+        return f(0,1)'''
+        dp[0][0] = 0
+        dp[0][1] = -prices[0]
+        for i in range(1,n):
+            dp[i][0] = max(prices[i]+dp[i-1][1],dp[i-1][0])
+            dp[i][1] = max(-prices[i]+dp[i-1][0],dp[i-1][1])
+        return max(dp[n-1][0],dp[n-1][1])
