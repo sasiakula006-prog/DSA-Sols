7d15138aa1d45b63ce8d7df9eac37dee9fd094b6
Sasi Akula <sasiakula006@gmail.com>
2026-06-14 14:31:01 +0530

    Time: 24 ms (5%), Space: 17.3 MB (7.49%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -14,9 +14,11 @@ class Solution(object):
                 dp[i][b] = max(prices[i]+f(i+1,1),f(i+1,0))
                 return dp[i][b]
         return f(0,1)'''
-        dp[0][0] = 0
-        dp[0][1] = -prices[0]
+        cur = prev = [-1]*2
+        prev[0] = 0
+        prev[1] = -prices[0]
         for i in range(1,n):
-            dp[i][0] = max(prices[i]+dp[i-1][1],dp[i-1][0])
-            dp[i][1] = max(-prices[i]+dp[i-1][0],dp[i-1][1])
-        return max(dp[n-1][0],dp[n-1][1])
+            cur[0] = max(prices[i]+prev[1],prev[0])
+            cur[1] = max(-prices[i]+prev[0],prev[1])
+            prev = cur
+        return max(prev[0],prev[1])
