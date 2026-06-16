94c2ecd5dcf2f0f4c3df3e614b916c1c986119ea
Sasi Akula <sasiakula006@gmail.com>
2026-06-16 17:08:02 +0530

    Time: 550 ms (85.37%), Space: 12.5 MB (100%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.py
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.py
@@ -5,7 +5,7 @@ class Solution(object):
         cuts.append(n)
         cuts.sort()
         l = len(cuts)
-        dp = [[-1]*(l) for _ in range(l)]
+        '''dp = [[-1]*(l) for _ in range(l)]
         def f(i,j):
             if i>j:
                 return 0 
@@ -17,4 +17,15 @@ class Solution(object):
                 mini = min(s,mini)
             dp[i][j] = mini
             return mini
-        return f(1,c)
+        return f(1,c)'''
+        dp = [[0]*(l) for _ in range(l)]
+        for i in range(c,0,-1):
+            for j in range(1,c+1):
+                if i>j:
+                    continue
+                mini = 1e9
+                for k in range(i,j+1):
+                    s = (cuts[j+1]-cuts[i-1])+dp[i][k-1]+dp[k+1][j]
+                    mini = min(s,mini)
+                dp[i][j] = mini
+        return dp[1][c]
