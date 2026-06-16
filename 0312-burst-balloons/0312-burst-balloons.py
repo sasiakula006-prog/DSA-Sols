8c241a189e250e4264190008f5ab4cfe07302d77
Sasi Akula <sasiakula006@gmail.com>
2026-06-16 17:53:00 +0530

    Time: 3192 ms (58.03%), Space: 14.4 MB (36.5%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.py
+++ 0312-burst-balloons/0312-burst-balloons.py
@@ -2,8 +2,7 @@ class Solution(object):
     def maxCoins(self, nums):
         nums = [1]+nums+[1]
         n = len(nums)
-        dp = [[-1]*n for _ in range(n)]
-        def f(i,j):
+        '''def f(i,j):
             if i>j:
                 return 0
             if dp[i][j] != -1:
@@ -12,7 +11,18 @@ class Solution(object):
             for k in range(i,j+1):
                 s = nums[i-1]*nums[k]*nums[j+1]+f(i,k-1)+f(k+1,j)
                 maxi = max(maxi,s)
-                dp[i][j] = maxi
+            dp[i][j] = maxi
             return maxi
-        return f(1,n-2)
+        return f(1,n-2)'''
+        dp = [[0]*n for _ in range(n)]
+        for i in range(n-2,0,-1):
+            for j in range(1,n-1):
+                if i>j:
+                    continue
+                maxi = 0
+                for k in range(i,j+1):
+                    s = nums[i-1]*nums[k]*nums[j+1] + dp[i][k-1] + dp[k+1][j]
+                    maxi = max(maxi,s)
+                dp[i][j] = maxi
+        return dp[1][n-2]
         
