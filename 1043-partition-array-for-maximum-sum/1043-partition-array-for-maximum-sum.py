c8c91359c0be2061a4b3c1dfc2c18e20fce202e8
Sasi Akula <sasiakula006@gmail.com>
2026-06-16 23:40:03 +0530

    Time: 267 ms (63.33%), Space: 12.3 MB (90%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.py
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.py
@@ -1,7 +1,7 @@
 class Solution(object):
     def maxSumAfterPartitioning(self, arr, k):
         n = len(arr)
-        dp = [-1]*n
+        '''dp = [-1]*n
         def f(i):
             if i == n:
                 return 0
@@ -16,4 +16,16 @@ class Solution(object):
                     maxi = max(maxi,s)
             dp[i] = maxi
             return maxi
-        return f(0)
+        return f(0)'''
+        dp = [-1]*(n+1)
+        dp[n] = 0
+        for i in range(n-1,-1,-1):
+            maxi = 0
+            mx = 0
+            for j in range(i,i+k):
+                if j<n:
+                    mx = max(mx,arr[j])
+                    s = mx*(j-i+1) + dp[j+1]
+                    maxi = max(maxi,s)
+            dp[i] = maxi
+        return dp[0]
