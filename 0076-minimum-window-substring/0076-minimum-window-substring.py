b3721a7b4b0973ffa31869fe6cd2200645bee702
Sasi Akula <sasiakula006@gmail.com>
2026-06-18 23:45:37 +0530

    Time: 87 ms (74.79%), Space: 12.5 MB (89.5%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def minWindow(self, s, t):
+        d = defaultdict(int)
+        for val in t:
+            d[val]+=1
+        cnt = 0
+        n = len(s)
+        si = -1
+        mini = 1e9
+        l = r = 0
+        while r<n:            
+            if d[s[r]] > 0:
+                cnt+=1
+            d[s[r]]-=1
+            while cnt==len(t):
+                if mini>(r-l):
+                    mini = r-l
+                    si = l
+                d[s[l]]+=1
+                if d[s[l]]>0:
+                    cnt-=1
+                l+=1
+            r+=1
+        if mini ==1e9:
+            return ''
+        return s[si:si+mini+1]
