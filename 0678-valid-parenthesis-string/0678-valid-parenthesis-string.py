bf0145747c9d8234abca9a61271dd945cedbe46f
Sasi Akula <sasiakula006@gmail.com>
2026-06-19 22:12:10 +0530

    Time: 3 ms (16.99%), Space: 12.4 MB (60.55%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.py
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.py
@@ -1,28 +1,16 @@
 class Solution(object):
     def checkValidString(self, s):
-        n=len(s)
-        dp = [[-1]*(n+1) for _ in range(n)]
-        def f(i,c):
-            if i==n:
-                if c==0:
-                    return True
-                else:
-                    return False
-            if c<0:
-                return False
-            if dp[i][c] !=-1:
-                return dp[i][c]
-            if s[i] == '(':
-                dp[i][c] = f(i+1,c+1)
-                return dp[i][c]
-            elif s[i] == ')':
-                dp[i][c] = f(i+1,c-1)
-                return dp[i][c]
+        mini,maxi = 0,0
+        for val in s:
+            if val =='(':
+                mini,maxi = mini+1,maxi+1
+            elif val =='*':
+                mini,maxi = max(0,mini-1),maxi+1
             else:
-                if f(i+1,c+1) or f(i+1,c) or f(i+1,c-1):
-                    dp[i][c] = True
-                    return True
+                if maxi:
+                    mini,maxi = max(0,mini-1),maxi-1
                 else:
-                    dp[i][c] = False
                     return False
-        return f(0,0)
+        if mini ==0:
+            return True
+        return False
