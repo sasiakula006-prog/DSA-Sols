43b3cb54559df3586903486fc9113c967c2ec917
Sasi Akula <sasiakula006@gmail.com>
2026-06-20 01:19:26 +0530

    Time: 6 ms (84.51%), Space: 13.3 MB (22.05%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -1,14 +1,13 @@
 class Solution(object):
     def jump(self, nums):
         n = len(nums)
-        dp = [-1]*n
-        dp[n-1] = 0
-        for i in range(n-2,-1,-1):
-            for j in range(nums[i],-1,-1):
-                if i+j<n and dp[i+j] !=-1:
-                    if dp[i]!=-1:
-                        dp[i] = min(dp[i],1+dp[i+j])
-                    else:
-                        dp[i] = 1+dp[i+j]
-        return dp[0]
+        farthest = 0
+        target_node = 0
+        jumps = 0
+        for i in range(n-1):
+            farthest = max(farthest,i+nums[i])
+            if i==target_node:
+                jumps +=1
+                target_node = farthest
+        return jumps
         
