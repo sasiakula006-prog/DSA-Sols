f4b8e9fe9d82526f5da5e7f198a8c181a6dc3352
Sasi Akula <sasiakula006@gmail.com>
2026-06-21 21:32:53 +0530

    Time: 23 ms (44.39%), Space: 14.6 MB (84.47%) - LeetHub

---
+++ 0135-candy/0135-candy.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def candy(self, ratings):
+        n = len(ratings)
+        left = [1]*n
+        right = [1]*n
+        for i in range(1,n):
+            if ratings[i]>ratings[i-1]:
+                left[i] = left[i-1]+1
+        
+        for i in range(n-2,-1,-1):
+            if ratings[i]> ratings[i+1]:
+                right[i] = right[i+1]+1
+        t = 0
+        for i in range(n):
+            t += max(left[i],right[i])
+
+        return t
