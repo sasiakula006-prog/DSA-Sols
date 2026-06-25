7361b8e2d91e0798bf28763f3cc0a73ed08b38be
Sasi Akula <sasiakula006@gmail.com>
2026-06-25 19:17:21 +0530

    Time: 0 ms (100%), Space: 12.6 MB (20.71%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.py
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.py
@@ -7,12 +7,24 @@
 class Solution(object):
     def postorderTraversal(self, root):
         ans= []
-        def dfs(cur):
+        '''def dfs(cur):
             if not cur:
                 return 
             dfs(cur.left)
             dfs(cur.right)
             ans.append(cur.val)
         dfs(root)
-        return ans
+        return ans'''
+        s1,s2 = [root],[]
+        while s1:
+            a = s1.pop()
+            if not a:
+                continue
+            s2.append(a)
+            s1.append(s2[-1].left)
+            s1.append(s2[-1].right)
         
+        while s2:
+            ans.append(s2.pop().val)
+
+        return ans
