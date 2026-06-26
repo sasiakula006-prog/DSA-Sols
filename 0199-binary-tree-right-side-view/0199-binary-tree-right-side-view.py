bcf177103bc3885acd393044ac6ce1e3063f0072
Sasi Akula <sasiakula006@gmail.com>
2026-06-26 23:17:04 +0530

    Time: 0 ms (100%), Space: 12.3 MB (69.4%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
@@ -6,18 +6,22 @@
 #         self.right = right
 class Solution(object):
     def rightSideView(self, root):
+        if not root:
+            return []
         d = {}
         q = deque([(root,0)])
+        mv = 0
         while q:
             n,v = q.popleft()
             if not n:
                 continue
+            mv = max(mv,v)
             if v not in d:
                 d[v] = n.val
             q.append((n.right,v+1))
             q.append((n.left,v+1))
         
-        ans = []
-        for i in sorted(d.keys()):
-            ans.append(d[i])
+        ans = [-1]*(mv+1)
+        for i in d.keys():
+            ans[i] = d[i]
         return ans
