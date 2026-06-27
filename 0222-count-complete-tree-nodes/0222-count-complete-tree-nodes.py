21424045ba1d444ab0c7b613fc288582bd2585d0
Sasi Akula <sasiakula006@gmail.com>
2026-06-27 21:19:53 +0530

    Time: 0 ms (100%), Space: 28.3 MB (13.66%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.py
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.py
@@ -5,26 +5,23 @@
 #         self.left = left
 #         self.right = right
 class Solution(object):
+    def lh(self,n):
+        cnt = 0
+        while n:
+            cnt +=1
+            n = n.left
+        return cnt
+    def rh(self,n):
+        cnt = 0
+        while n:
+            cnt +=1
+            n = n.right
+        return cnt
     def countNodes(self, root):
-        def lh(n):
-            cnt = 0
-            while n:
-                cnt +=1
-                n = n.left
-            return cnt
-        
-        def rh(n):
-            cnt = 0
-            while n:
-                cnt +=1
-                n = n.right
-            return cnt
-        def dfs(cur):
-            if not cur:
-                return 0
-            l = lh(cur)
-            r = rh(cur)
-            if lh == rh:
-                return 2**(lh)-1
-            return 1+dfs(cur.left)+dfs(cur.right)
-        return dfs(root)
+        if not root:
+            return 0
+        l = self.lh(root)
+        r = self.rh(root)
+        if l == r:
+            return 2**(l)-1
+        return 1+self.countNodes(root.left)+self.countNodes(root.right)
