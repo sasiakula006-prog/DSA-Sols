a7a2840dbfc041f48a9d01c8d99197168dce3a70
Sasi Akula <sasiakula006@gmail.com>
2026-06-27 16:14:49 +0530

    Time: 0 ms (100%), Space: 14 MB (32.88%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.py
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.py
@@ -9,15 +9,14 @@ class Solution(object):
         q = deque([(root,1)])
         ans = 0
         while q:
-            maxi = 0
-            mini = float('inf')
+            _,mini = q[0]
+            maxi = mini 
             for _ in range(len(q)):
                 n,i = q.popleft()
+                maxi = i
                 if n.left:
                     q.append((n.left,2*i))
                 if n.right:
                     q.append((n.right,2*i+1))
-                maxi = max(maxi,i)
-                mini = min(mini,i)
                 ans = max(ans,maxi-mini+1)
         return ans
