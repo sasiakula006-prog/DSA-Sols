5cbf1949579ecaf72a7897aaa07eb32d44072f46
Sasi Akula <sasiakula006@gmail.com>
2026-07-04 14:30:43 +0530

    Time: 314 ms (69.81%), Space: 70.7 MB (69.81%) - LeetHub

---
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def minScore(self, n, roads):
+        g = [[] for _ in range(n+1)]
+        for u0,v0,d0 in roads:
+            g[u0].append((v0,d0))
+            g[v0].append((u0,d0))
+        minp = float('inf')
+        vis = set()
+        q = deque([1])
+        while q:
+            u = q.popleft()
+            for v,d in g[u]:
+                minp = min(minp,d) 
+                if v not in vis:
+                    vis.add(v)
+                    q.append(v)
+
+        return minp
+                    
