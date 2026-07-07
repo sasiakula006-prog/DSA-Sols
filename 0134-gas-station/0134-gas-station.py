2413c13005f947e36064e175a5d3566c66696446
Sasi Akula <sasiakula006@gmail.com>
2026-07-08 02:34:43 +0530

    Time: 27 ms (55.63%), Space: 15.4 MB (100%) - LeetHub

--- 0134-gas-station/0134-gas-station.py
+++ 0134-gas-station/0134-gas-station.py
@@ -1,27 +1,16 @@
 class Solution(object):
     def canCompleteCircuit(self, gas, cost):
+        if sum(gas)<sum(cost):
+            return -1
         n = len(gas)
-        def f(i):
-            j = i
-            f = 0
-            while j<i+n:
-                f += gas[j%n]-cost[j%n]
-                if f<0:
-                    break
-                j+=1            
-            return j%n
-        
+        f = 0
+        ans = 0
         i = 0
         while i<n:
-            if gas[i]>=cost[i]:
-                v = f(i)
-                if v == i:
-                    return i
-                if v<i:
-                    return -1
-                i = v
-                continue
+            f += gas[i]-cost[i]
+            if f<0:
+                ans = (i+1)
+                f = 0
             i+=1
-        return -1
-        
+        return ans
         
