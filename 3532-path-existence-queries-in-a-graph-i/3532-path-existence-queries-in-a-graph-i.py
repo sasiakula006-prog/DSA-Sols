47324bddc37d40100de72cacd38a72a8448d94d5
Sasi Akula <sasiakula006@gmail.com>
2026-07-09 14:32:27 +0530

    Time: 135 ms (54.55%), Space: 46.1 MB (9.09%) - LeetHub

--- 3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.py
+++ 3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.py
@@ -1,32 +1,14 @@
 class Solution(object):
     def pathExistenceQueries(self, n, nums, maxDiff, queries):
-        size = [1]*n
         parent = [i for i in range(n)]
-        def find(i):
-            if parent[i]==i:
-                return i
-            parent[i] = find(parent[i])
-            return parent[i]
-
-        def join(u,v):
-            pu = find(u)
-            pv = find(v)
-            if pu==pv:
-                return
-            if size[pu]>=size[pv]:
-                parent[pv] = pu
-                size[pu] += size[pv]
-            elif size[pu] < size[pv]:
-                parent[pu] = pv
-                size[pv] += size[pu]
         
         for u in range(1,n):
             if nums[u]-nums[u-1]<=maxDiff:
-                join(u,u-1)
+                parent[u] = parent[parent[u-1]]
             
         ans = []
         for u1,v1 in queries:
-            if find(u1)==find(v1):
+            if parent[u1]==parent[v1]:
                 ans.append(True)
             else:
                 ans.append(False)
