c62f035788e425e3e3a44955182d513c30e36fb9
Sasi Akula <sasiakula006@gmail.com>
2026-07-16 00:45:25 +0530

    Time: 184 ms (89.47%), Space: 100.8 MB (48.7%) - LeetHub

--- 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.py
+++ 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.py
@@ -1,30 +1,26 @@
-class Solution(object):
-    def criticalConnections(self, n, connections):
-        if len(connections) <= n-1:
-            return connections
-        adj = [[] for _ in range(n)]
-        for u0,v0 in connections:
-            adj[u0].append(v0)
-            adj[v0].append(u0)
-        time = [1]*n
-        l_t = [1]*n
-        vis = [False]*n
+class Solution:
+    def criticalConnections(self, n: int, connections: List[List[int]]) -> List[List[int]]:
+        adj = [[]for _ in range(n)]
+        for u,v in connections:
+            adj[u].append((v))
+            adj[v].append((u))
+        time = [-1]*n
+        l_time = [-1]*n
+        time[0] = l_time[0] = 0
         ans = []
+        vis = set()
         def dfs(u,p):
-            vis[u] = True
+            vis.add(u)
             for v in adj[u]:
                 if v==p:
                     continue
-                elif not vis[v]:
-                    l_t[v] = time[v] = time[u]+1
+                if v not in vis:
+                    time[v] = l_time[v] = time[u]+1
                     dfs(v,u)
-                    l_t[u] = min(l_t[u],l_t[v])
-                    if time[u]<l_t[v]:
+                    l_time[u] = min(l_time[u],l_time[v])
+                    if time[u]<l_time[v]:
                         ans.append([u,v])
                 else:
-                    l_t[u] = min(l_t[u],l_t[v])
+                    l_time[u] = min(l_time[u],l_time[v])
         dfs(0,-1)
         return ans
-
-
-        
