7cc5b4efd1c6c4e5387dee7872b0695a91b22bab
Sasi Akula <sasiakula006@gmail.com>
2026-07-17 15:32:32 +0530

    Time: 876 ms (12.5%), Space: 40 MB (62.5%) - LeetHub

---
+++ 3312-sorted-gcd-pair-queries/3312-sorted-gcd-pair-queries.py
@@ -0,0 +1,41 @@
+class Solution:
+    def gcdValues(self, nums: List[int], queries: List[int]) -> List[int]:
+        n = len(nums)
+        gcdPairs = []
+        m = max(nums)
+        a = [0]*(m+1)
+        for num in nums:
+            a[num]+=1
+
+        cnt = [0]*(m+1)
+        for g in range(m,0,-1):
+            c = 0
+            for d in range(g,m+1,g):
+                c += a[d]
+            cnt[g] = (c*(c-1))/2
+            for mu in range(2*g,m+1,g):
+                cnt[g] -= cnt[mu]
+
+        for i in range(1,m+1):
+            cnt[i] += cnt[i-1]
+        
+        def lowb(val):
+            lo,hi = 1,m
+            res = hi
+            while lo<=hi:
+                mid = (lo+hi)//2
+                if cnt[mid]>val:
+                    res = mid
+                    hi = mid-1
+                else:
+                    lo = mid+1
+            return res
+        
+        l = len(queries)
+        ans = [0]*l
+        for i in range(l):
+            ans[i] = lowb(queries[i])
+        return ans
+                
+            
+        
