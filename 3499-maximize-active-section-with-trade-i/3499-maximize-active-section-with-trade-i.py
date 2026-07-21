8020d66b9ad4ebf64e2ddcb04ed6442e3d9f7544
Sasi Akula <sasiakula006@gmail.com>
2026-07-22 00:29:33 +0530

    Time: 767 ms (42.05%), Space: 20.8 MB (72.9%) - LeetHub

--- 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.py
+++ 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.py
@@ -1,22 +1,19 @@
 class Solution:
     def maxActiveSectionsAfterTrade(self, s: str) -> int:
-        cnt1 = 0
+        cnt1,cnt0 = 0,0
         n = len(s)
         for val in s:
             if val =='1':
                 cnt1 +=1
-        zerob = []
         i=0
+        p = float('-inf')
         while i<n:
             if s[i] == '0':
                 st = i
                 while i<n and s[i] == s[st]:
                     i+=1
-                zerob.append(i-st)
+                c = i - st
+                cnt0 = max(cnt0,p+c)
+                p = c
             i+=1
-        if len(zerob) <2:
-            return cnt1
-        cnt0 = 0
-        for i in range(len(zerob)-1):
-            cnt0 = max(cnt0,zerob[i]+zerob[i+1])
         return cnt1+cnt0
