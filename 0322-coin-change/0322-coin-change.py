18bad484e207ba7dac89e8a4b95f7a7e3df425ac
Sasi Akula <sasiakula006@gmail.com>
2026-08-03 18:17:52 +0530

    Time: 1132 ms (8.89%), Space: 70.8 MB (11.71%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,41 +1,22 @@
-class Solution(object):
-    def coinChange(self, coins, amount):
-        l = len(coins)
-        dp = [[-1]*(amount+1) for _ in range(l)]
-        '''def f(i,a):
-            if i ==l-1:
-                if not a%coins[-1]:
-                    return a/coins[-1]
-                else:
-                    return 1e9
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        coins.sort()
+        n = len(coins)
+        dp = [[-1]*(amount+1) for _ in range(n)]
+        inf = float('inf')
+        def f(i,a):
+            if a==0:
+                return 0
+            if i==n:
+                return inf
             if dp[i][a] !=-1:
                 return dp[i][a]
-            p1 = f(i+1,a)
-            p2 = 1e9
-            if a>= coins[i]:
-                p2 = 1 + f(i,a-coins[i])
+            p1,p2 = f(i+1,a),inf
+            if a>=coins[i]:
+                p2 = 1+f(i,a-coins[i])
             dp[i][a] = min(p1,p2)
             return min(p1,p2)
         ans = f(0,amount)
-        if ans >= 1e9:
-            return -1
-        return ans'''
-
-#tablation
-        for t in range(amount+1):
-            if not t%coins[0]:
-                dp[0][t] = t/coins[0]
-            else:
-                dp[0][t] = 1e9
-
-        for i in range(1,l):
-            for a in range(amount+1):
-                p1 = 0 + dp[i-1][a]
-                p2 = 1e9
-                if coins[i]<=a:
-                    p2 = 1+dp[i][a-coins[i]]
-                dp[i][a] = min(p1,p2)
-        ans = dp[l-1][amount]
-        if ans >= 1e9:
+        if ans >= inf:
             return -1
         return ans
