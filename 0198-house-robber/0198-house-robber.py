e1c19d4f163c63da3d28cbf3f7c2cbcff57b6465
Sasi Akula <sasiakula006@gmail.com>
2026-08-04 18:37:22 +0530

    Time: 3 ms (4.88%), Space: 19.5 MB (6.05%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,24 +1,13 @@
-class Solution(object):
-    def rob(self, nums):
+class Solution:
+    def rob(self, nums: List[int]) -> int:
         n = len(nums)
-        if n==1:
-            return nums[-1]
-        # recursive sol got runtime error tho
-        '''ans = [0]
-        def solve(i,s,ans):
-            if i<n-2:
-                for j in range(2,n-i):
-                    solve(i+j,s+nums[i+j],ans)
-            else:
-                if s>ans[-1]:
-                    ans.append(s)
-        solve(0,nums[0],ans)
-        solve(1,nums[1],ans)
-        return ans[-1]'''
-        prev1  = 0
-        prev2 = 0
-        for val in nums:
-            cur = max(prev1,prev2+val)
-            prev2 = prev1
-            prev1 = cur
-        return cur
+        dp = [-1]*n
+        def f(i):
+            if i>=n:
+                return 0
+            if dp[i] !=-1:
+                return dp[i]
+            p1,p2 = f(i+1),nums[i]+f(i+2)
+            dp[i] = max(p1,p2)
+            return max(p1,p2)
+        return f(0)
